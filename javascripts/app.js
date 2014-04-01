aaf36268bdffa86c05db382ae3fb2cf97a43ea82
thenickcox <nick@nickcox.me>
2014-04-01 07:36:06 -0700

    initial commit: firebase bindings functioning

---
+++ javascripts/app.js
@@ -0,0 +1,48 @@
+window.App = Ember.Application.create({});
+
+App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase("https://luminous-fire-4173.firebaseio.com/")
+});
+App.ApplicationSerializer = DS.FirebaseSerializer.extend();
+
+App.Router.map(function(){
+  this.resource('items', { path: '/' });
+});
+
+App.Item = DS.Model.extend({
+  title: DS.attr(),
+  isBought: DS.attr()
+});
+
+App.Item.FIXTURES = [
+  {
+    id: 1,
+    title: 'carrots'
+  },
+  {
+    id: 2,
+    title: 'celery'
+  }
+];
+
+App.ItemsRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.find('item');
+  }
+});
+
+
+App.ItemsController = Ember.ArrayController.extend({
+  actions: {
+      createItem: function(){
+        var title = this.get('itemTitle');
+        if (!title || !title.trim()) { return };
+        var item = this.store.createRecord('item', {
+          title: title,
+          isBought: false
+        });
+        this.set('itemTitle', '');
+        item.save();
+      }
+    }
+});
